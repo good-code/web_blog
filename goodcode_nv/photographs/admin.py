45c137d25bc5a5450bd45d9e9e707217fbebf5e0
Arek Tymieniecki <arek@goodcode.co.uk>
2013-01-27 21:51:23 +0000

    clened up files and added admin.py for photographs module

---
+++ goodcode_nv/photographs/admin.py
@@ -0,0 +1,25 @@
+from reversion.admin import VersionAdmin
+from models import Photograph, Album
+from django.contrib import admin
+
+class PhotographOptions(admin.ModelAdmin):
+   pass
+
+class AlbumOptions(admin.ModelAdmin):
+   pass
+
+
+#class VersionedPostAdmin(VersionAdmin, PostOptions):
+#   ordering = ('-created','title','sku')
+#   list_display = ('title', 'sku', 'active', 'created', 'modified')
+#   list_filter = ('active',)
+#   search_fields = ['sku', 'name']
+#
+#class VersionedFortuneAdmin(VersionAdmin, FortuneOptions):
+#   ordering = ('name','slug')
+#   list_display = ('name', 'slug', 'active', 'created',)
+#   list_filter = ('active',)
+#   search_fields = ['slug', 'name']
+#
+admin.site.register(Photograph, PhotographAdmin)
+admin.site.register(Album, AlbumAdmin)
